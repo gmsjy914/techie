03a4339740fe2c564ca183263153d6d8ca2a568f
yunsungjoong <tjdwnd0914@naver.com>
2023-09-05 16:15:40 +0900

    회원 개인페이지 및 개인정보수정 페이지와 암호화 업데이트

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,11 @@ from django.db import models
 
 class Registration(models.Model):
     email = models.CharField(max_length=255, null=False)
+
+
+class Profile(models.Model):
+    nickname = models.CharField(max_length=100, null=False, unique=True)
+    image = models.ImageField(upload_to="profile/", null=True)
+    message = models.CharField(max_length=200, null=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import Registration
 
 
@@ -46,3 +49,29 @@ class AccountLoginView(LoginView):
 
 class AccountLogoutView(LogoutView):
     pass
+
+
+class AccountDetailView(DetailView):
+    model = User
+    template_name = "accountapp/detail.html"
+    context_object_name = "target_user"
+
+
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    context_object_name = "target_user"
+    template_name = "accountapp/update.html"
+
+    def get_success_url(self):
+        return reverse("accountapp:detail",
+                       kwargs={"pk": self.kwargs["pk"]})
+
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = "target_user"
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = "accountapp/derete"
