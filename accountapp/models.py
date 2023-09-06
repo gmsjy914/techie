466ebe0ce2fba05dd98e2c905a79e5ffbb335365
yunsungjoong <tjdwnd0914@naver.com>
2023-09-06 10:30:21 +0900

    model폼 업데이트

--- accountapp/models.py
+++ accountapp/models.py
@@ -6,11 +6,3 @@ from django.db import models
 
 class Registration(models.Model):
     email = models.CharField(max_length=255, null=False)
-
-
-class Profile(models.Model):
-    nickname = models.CharField(max_length=100, null=False, unique=True)
-    image = models.ImageField(upload_to="profile/", null=True)
-    message = models.CharField(max_length=200, null=True)
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
--- accountapp/urls.py
+++ accountapp/urls.py
@@ -17,4 +17,7 @@ urlpatterns = [
 
     path('detail/<int:pk>/', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>/', AccountUpdateView.as_view(), name='update'),
+    path('update/<int:pk>/', AccountUpdateView.as_view(), name='update'),
+
+
 ]
