075fed151e55ac5a83b06dc7815a98c7181fe3fb
yunsungjoong <tjdwnd0914@naver.com>
2023-09-07 15:33:45 +0900

    게시글 변경 및 수정 업데이트

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,11 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
 
 
 class ArticleForm(ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '제목'}))
     class Meta:
         model = Article
         fields = [
