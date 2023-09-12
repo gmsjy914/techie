dca02242650eff3a129db50343a8f8fea752b9d3
yunsungjoong <tjdwnd0914@naver.com>
2023-09-12 10:51:21 +0900

    뎃글창 생성 업데이트

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from commentapp.form import CommentCreationForm
+from commentapp.models import Comment
+
 
 # Create your views here.
+
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentCreationForm
+    template_name = "commentapp/create.html"
+    
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('articleapp:detail',
+                       kwargs={'pk': self.object.article.pk})
