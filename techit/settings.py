5e68e87b154e876f779b109b34e1e4b9524add83
yunsungjoong <tjdwnd0914@naver.com>
2023-09-12 15:16:23 +0900

    모바일  업데이트

--- techit/settings.py
+++ techit/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-u2y_$t!rpe3k)flgpi--q!!5e75l751xoctt!8g$@a2%tdyl=+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
