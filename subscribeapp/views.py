9b4c27ca05937f1c7cb66885a42a67d1e47dae12
yunsungjoong <tjdwnd0914@naver.com>
2023-09-13 13:39:12 +0900

    구독취소 업데이트

---
+++ subscribeapp/views.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import RedirectView
+
+from subscribeapp.models import Subscription
+
+
+# Create your views here.
+
+
+class SubscribeView(RedirectView):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        target_user = User.objects.get(pk=self.kwargs['pk'])
+
+        subs = Subscription.objects.filter(user=user, target_user = target_user)
+
+        if subs:
+            subs.delete()
+        else:
+            Subscription(user=user, target_user=target_user).save()
+
+
+        return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('accountapp:detail', kwargs={'pk': self.kwargs['pk']})
+
+
+
+
+
+
+
+
