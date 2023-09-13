2fa53b884864d7388ff8ab6cfb6c2ee093922242
yunsungjoong <tjdwnd0914@naver.com>
2023-09-13 14:22:55 +0900

    구독창 업데이트

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from subscribeapp.views import SubscribeView
+from subscribeapp.views import SubscribeView, SubscriptionListView
 
 app_name = 'subscribeapp'
 
@@ -8,4 +8,5 @@ app_name = 'subscribeapp'
 urlpatterns = [
 
     path('sub/<int:pk>/', SubscribeView.as_view(), name='sub'),
+    path('list/', SubscriptionListView.as_view(), name='list'),
 ]
--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from subscribeapp.models import Subscription
 
 
@@ -14,13 +15,12 @@ class SubscribeView(RedirectView):
         user = request.user
         target_user = User.objects.get(pk=self.kwargs['pk'])
 
-        subs = Subscription.objects.filter(user=user, target_user = target_user)
+        subs = Subscription.objects.filter(user=user, target_user=target_user)
 
         if subs:
             subs.delete()
         else:
             Subscription(user=user, target_user=target_user).save()
-
 
         return super().get(request, *args, **kwargs)
 
@@ -28,9 +28,13 @@ class SubscribeView(RedirectView):
         return reverse('accountapp:detail', kwargs={'pk': self.kwargs['pk']})
 
 
+class SubscriptionListView(ListView):
+    model= Article
+    context_object_name = 'article_list'
+    template_name = 'subcribeapp/list.html'
+    paginate_by = 20
 
-
-
-
-
-
+    def get_queryset(self):
+        target_user_list = Subscription.objects.filter(user=self.request.user).values_list('target_user')
+        article_list = Article.objects.filter(writer__in=target_user_list)
+        return article_list
